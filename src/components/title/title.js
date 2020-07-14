368ee0a958bd5220065c00141ab2b3f2ed59d349
Hannah Lee <leexhannah@gmail.com>
2020-07-13 21:40:33 -0700

    disable layout.css

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 
 const Title = ({ title }) => (
   <Fade bottom duration={1000} delay={300} distance="0px">
-    <h2 className="text-3xl font-bold uppercase text-center tracking-wider text-orange-800">
+    <h2 className="text-3xl text-center tracking-wider text-orange-800">
       {title}
     </h2>
   </Fade>
