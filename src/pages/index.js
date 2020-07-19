f1789da98b2f8c63ade96f6ce907d2659c1e9dae
Hannah Lee <leexhannah@gmail.com>
2020-07-18 19:42:11 -0700

    remove App.js and use index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,44 @@
-import React from "react"
-import App from "../components/App.js"
+import React, { useState, useEffect } from "react"
+import { PortfolioProvider } from "../context/context"
+import Layout from "../components/layout"
+import Hero from "../components/hero/hero"
+import Projects from "../components/projects/projects"
+import About from "../components/about/about"
+import Contact from "../components/contact/contact"
+import "../css/global.css"
+import {
+  heroData,
+  aboutData,
+  projectsData,
+  contactData,
+  footerData,
+} from "../data/data"
 
-const IndexPage = () => <App />
+const IndexPage = () => {
+  const [hero, setHero] = useState({})
+  const [projects, setProjects] = useState([])
+  const [about, setAbout] = useState({})
+  const [contact, setContact] = useState({})
+  const [footer, setFooter] = useState({})
+
+  useEffect(() => {
+    setHero({ ...heroData })
+    setProjects([...projectsData])
+    setAbout({ ...aboutData })
+    setContact({ ...contactData })
+    setFooter({ ...footerData })
+  }, [])
+
+  return (
+    <Layout>
+      <PortfolioProvider value={{ hero, projects, about, contact, footer }}>
+        <Hero />
+        <Projects />
+        <About />
+        <Contact />
+      </PortfolioProvider>
+    </Layout>
+  )
+}
 
 export default IndexPage
