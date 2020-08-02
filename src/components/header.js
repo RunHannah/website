692c2466a288433072192ae4a500e31ca2d95d05
Hannah Lee <leexhannah@gmail.com>
2020-08-02 16:58:03 -0700

    add links to header

--- src/components/header.js
+++ src/components/header.js
@@ -10,13 +10,28 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 const Header = () => (
   <header className="mb-6">
     <div className="flex flex-row justify-end my-4 mr-6">
-      <Link to="/" className="socialIcon text-xl lg:text-2xl mx-4">
+      <Link
+        to="https://github.com/runhannah"
+        target="_blank"
+        aria-label="github"
+        className="socialIcon text-xl lg:text-2xl mx-4"
+      >
         <FontAwesomeIcon icon={faGithub} />
       </Link>
-      <Link to="/" className="socialIcon text-xl lg:text-2xl mx-4">
+      <Link
+        to="https://www.linkedin.com/in/hannahlee1/"
+        target="_blank"
+        aria-label="linkedin"
+        className="socialIcon text-xl lg:text-2xl mx-4"
+      >
         <FontAwesomeIcon icon={faLinkedinIn} />
       </Link>
-      <Link to="/" className="socialIcon text-xl lg:text-2xl mx-4">
+      <Link
+        to="https://twitter.com/yearofthecode"
+        target="_blank"
+        aria-label="twitter"
+        className="socialIcon text-xl lg:text-2xl mx-4"
+      >
         <FontAwesomeIcon icon={faTwitter} />
       </Link>
     </div>
