7109aab869bbfc36b4999858df615cb3ffe65615
Hannah Lee <leexhannah@gmail.com>
2020-08-04 19:44:50 -0700

    add fade to header and footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,6 +7,7 @@ import {
 } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Link } from "react-scroll"
+import Fade from "react-reveal/Fade"
 import PortfolioContext from "../../context/context"
 
 const Footer = () => {
@@ -15,7 +16,7 @@ const Footer = () => {
 
   return (
     <footer className="text-center">
-      <span>
+      <Fade bottom duration={1000} delay={500} distance="30px">
         <Link
           to="hero"
           smooth
@@ -24,43 +25,43 @@ const Footer = () => {
         >
           <FontAwesomeIcon icon={faAngleUp} />
         </Link>
-      </span>
-      <div className="flex flex-row my-4 justify-center">
-        {networks &&
-          networks.map(network => {
-            const { id, name, url } = network
-            let fontName
+        <div className="flex flex-row my-4 justify-center">
+          {networks &&
+            networks.map(network => {
+              const { id, name, url } = network
+              let fontName
 
-            switch (name) {
-              case "twitter":
-                fontName = faTwitter
-                break
-              case "linkedin":
-                fontName = faLinkedinIn
-                break
-              case "github":
-                fontName = faGithub
-                break
-              default:
-                fontName = faCode
-            }
+              switch (name) {
+                case "twitter":
+                  fontName = faTwitter
+                  break
+                case "linkedin":
+                  fontName = faLinkedinIn
+                  break
+                case "github":
+                  fontName = faGithub
+                  break
+                default:
+                  fontName = faCode
+              }
 
-            return (
-              <a
-                key={id}
-                href={url}
-                rel="noopener noreferrer"
-                target="_blank"
-                aria-label={name}
-                className="socialIcon text-xl lg:text-2xl mx-4"
-              >
-                <FontAwesomeIcon icon={fontName} />
-              </a>
-            )
-          })}
-      </div>
+              return (
+                <a
+                  key={id}
+                  href={url}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  aria-label={name}
+                  className="socialIcon text-xl lg:text-2xl mx-4"
+                >
+                  <FontAwesomeIcon icon={fontName} />
+                </a>
+              )
+            })}
+        </div>
 
-      <p className="made mb-4 text-sm">Built with Gatsby and Tailwind CSS</p>
+        <p className="made mb-4 text-sm">Built with Gatsby and Tailwind CSS</p>
+      </Fade>
     </footer>
   )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Fade from "react-reveal/Fade"
 import {
   faGithub,
   faLinkedinIn,
@@ -8,35 +9,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Header = () => (
   <header>
-    <div className="flex flex-row justify-end my-4 mr-6">
-      <a
-        href="https://github.com/runhannah"
-        target="_blank"
-        rel="noreferrer"
-        aria-label="github"
-        className="socialIcon text-xl lg:text-2xl mx-4"
-      >
-        <FontAwesomeIcon icon={faGithub} />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/hannahlee1/"
-        target="_blank"
-        rel="noreferrer"
-        aria-label="linkedin"
-        className="socialIcon text-xl lg:text-2xl mx-4"
-      >
-        <FontAwesomeIcon icon={faLinkedinIn} />
-      </a>
-      <a
-        href="https://twitter.com/yearofthecode"
-        target="_blank"
-        rel="noreferrer"
-        aria-label="twitter"
-        className="socialIcon text-xl lg:text-2xl mx-4"
-      >
-        <FontAwesomeIcon icon={faTwitter} />
-      </a>
-    </div>
+    <Fade right duration={1000} delay={500} distance="30px">
+      <div className="flex flex-row justify-end my-4 mr-6">
+        <a
+          href="https://github.com/runhannah"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="github"
+          className="socialIcon text-xl lg:text-2xl mx-4"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/hannahlee1/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="linkedin"
+          className="socialIcon text-xl lg:text-2xl mx-4"
+        >
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </a>
+        <a
+          href="https://twitter.com/yearofthecode"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="twitter"
+          className="socialIcon text-xl lg:text-2xl mx-4"
+        >
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+      </div>
+    </Fade>
   </header>
 )
 
