7f807f8fbb4b6efe2bc2a5c1a91091873840c0ab
Hannah Lee <leexhannah@gmail.com>
2020-08-12 22:31:41 -0700

    fix fontSize, add fade to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,21 +2,24 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Fade from "react-reveal/Fade"
 import "../css/global.css"
 
 const NotFoundPage = () => (
   <Layout>
-    <div className="flex flex-col justify-center items-center h-screen">
-      <SEO title="404: Not found" />
-      <h1 className="text-4xl mb-8">PAGE NOT FOUND</h1>
-      <p className="mb-8">You just hit a route that doesn&#39;t exist.</p>
-      <Link
-        className="btn-text text-center text-xs py-3 px-4 sm:text-sm md:w-32 md:text-base md:py-4 md:mr-5 lg:text-lg lg:px-5 border-solid border-2 rounded font-bold"
-        to="/"
-      >
-        Go Home
-      </Link>
-    </div>
+    <Fade duration={1000} delay={500}>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <SEO title="404: Not found" />
+        <h1 className="text-3xl md:text-5xl mb-8">PAGE NOT FOUND</h1>
+        <p className="mb-8">You just hit a route that doesn&#39;t exist.</p>
+        <Link
+          className="btn-text text-center text-xs py-3 px-4 sm:text-sm md:w-32 md:text-base md:py-4 md:mr-5 lg:text-lg lg:px-5 border-solid border-2 rounded font-bold"
+          to="/"
+        >
+          Go Home
+        </Link>
+      </div>
+    </Fade>
   </Layout>
 )
 
