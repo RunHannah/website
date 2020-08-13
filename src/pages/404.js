601d6484605b8a4f1af5c2bc4446ccb52aa7398e
Hannah Lee <leexhannah@gmail.com>
2020-08-12 22:14:56 -0700

    style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,22 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "../css/global.css"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist.</p>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <SEO title="404: Not found" />
+      <h1 className="text-4xl mb-8">PAGE NOT FOUND</h1>
+      <p className="mb-8">You just hit a route that doesn&#39;t exist.</p>
+      <Link
+        className="btn-text text-center text-xs py-3 px-4 sm:text-sm md:w-32 md:text-base md:py-4 md:mr-5 lg:text-lg lg:px-5 border-solid border-2 rounded font-bold"
+        to="/"
+      >
+        Go Home
+      </Link>
+    </div>
   </Layout>
 )
 
